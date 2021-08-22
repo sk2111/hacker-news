4aff360b17c47007d3181566a85c422a02bbaa64
sk2111 <sathisht1996@gmail.com>
2021-08-22 12:40:02 +0530

    feat : Add story detail component
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- src/pages/StoryDetail/StoryDetail.js
+++ src/pages/StoryDetail/StoryDetail.js
@@ -1,12 +1,22 @@
+import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 //styles
 import styles from './StoryDetail.module.scss';
+//componets
+import Button from 'components/Button/Button';
+//router
+import { STORIES_PATH } from 'utilities/router/routePath';
 //selectors
 import { selectStoryById } from 'redux/rootSelector';
 
 const StoryDetail = ({ match }) => {
   const storyId = match.params.id;
+  const history = useHistory();
   const story = useSelector((state) => selectStoryById(state, storyId));
+
+  const handleGoBack = () => {
+    history.push(STORIES_PATH);
+  };
 
   if (!story) {
     return <h1 className={styles.error}>No Data Found!</h1>;
@@ -20,7 +30,17 @@ const StoryDetail = ({ match }) => {
       <div className={styles.bottomSection}>
         <div className={styles.mainInfo}>
           <h1 className={styles.storyTitle}>{story.title}</h1>
+          <div className={styles.subInfo}>
+            <p>Score : {story.score}</p>
+            <p>Posted At : {story.date}</p>
+            <p>Posted By : {story.by}</p>
+          </div>
         </div>
+      </div>
+      <div className={styles.backBtnContainer}>
+        <Button className={styles.backBtn} onClick={handleGoBack}>
+          Go Back
+        </Button>
       </div>
     </div>
   );
