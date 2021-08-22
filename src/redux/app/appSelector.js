fa49fe83adb86b10fe5d846d3b2fa543986bc1f2
sk2111 <sathisht1996@gmail.com>
2021-08-22 09:08:28 +0530

    feat : Add search story feature
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- src/redux/app/appSelector.js
+++ src/redux/app/appSelector.js
@@ -6,10 +6,26 @@ export const selectStories = createSelector(
   [selectAppSlice],
   (app) => app.stories
 );
-export const selectAllStoryIds = createSelector(
-  [selectStories],
-  (stories) => stories.allIds
+
+export const selectSearchStory = createSelector(
+  [selectAppSlice],
+  (app) => app.searchStory
 );
+
+export const selectAllStoryIds = createSelector(
+  [selectStories, selectSearchStory],
+  (stories, searchTerm) => {
+    //handle Search
+    const storyIds = stories.allIds;
+    const allStories = stories.byId;
+    const searchIds = storyIds.filter((id) => {
+      const title = allStories[id]?.title.toLowerCase() ?? '';
+      return title.includes(searchTerm.toLowerCase());
+    });
+    return searchIds;
+  }
+);
+
 export const selectStoryById = createSelector(
   selectStories,
   (_, id) => id,
